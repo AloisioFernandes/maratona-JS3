e1455bf9c86510eddc4b964362e59083e6dac1cf
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-10-10 19:56:26 -0300

    Estrutura inicial.

---
+++ typing/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched"></span>
+        <span className="remainder"></span>
+      </div>
+      <div className="typed-keys"></div>
+      <div className="completed-words">
+        <ol>
+          <li></li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default App
