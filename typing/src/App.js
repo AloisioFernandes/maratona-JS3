16bf0a52ce35ed6810c42d9ba2e8087728204462
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-10-11 20:39:27 -0300

    Estilização inicial.

--- typing/src/App.js
+++ typing/src/App.js
@@ -4,13 +4,15 @@ const App = () => {
   return (
     <div className="container">
       <div className="valid-keys">
-        <span className="matched"></span>
-        <span className="remainder"></span>
+        <span className="matched">emer</span>
+        <span className="remainder">son</span>
       </div>
-      <div className="typed-keys"></div>
+      <div className="typed-keys">asdfemerasdf</div>
       <div className="completed-words">
         <ol>
-          <li></li>
+          <li>cidade</li>
+          <li>carro</li>
+          <li>profissional</li>
         </ol>
       </div>
     </div>
