6b9e72f2d5e212acf70341cb654f2a39e0b06118
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-10-14 21:21:04 -0300

    Verificando a tecla digitada.

--- typing/src/App.js
+++ typing/src/App.js
@@ -10,8 +10,31 @@ const getWord = () => {
   return word.toLowerCase()
 }
 
+const isValidKey = (key, word) => {
+  if(!word) return false
+
+  const result = word.split('').includes(key)
+  return result
+}
+
+const Word = ({word, validKeys}) => {
+  if(!word) return null
+  
+  const joinedKeys = validKeys.join('')
+  const matched = word.slice(0, joinedKeys.length)
+  const remainder = word.slice(joinedKeys.length)
+
+  return (
+    <>
+      <span className="matched">{matched}</span>
+      <span className="remainder">{remainder}</span>
+    </>
+  )
+}
+
 const App = () => {
   const [typedKeys, setTypedKeys] = useState([])
+  const [validKeys, setValidKeys] = useState([])
   const [word, setWord] = useState('')
 
   useEffect(() => {
@@ -22,13 +45,20 @@ const App = () => {
     e.preventDefault()
     const { key } = e;
     setTypedKeys((prevTypedKeys) => [...prevTypedKeys, key].slice(MAX_TYPED_KEYS * -1))
+
+    if(isValidKey(key, word)) {
+      setValidKeys((prev) => {
+        const isValidLength = prev.length <= word.length
+        const isNextChar = isValidLength && word[prev.length] === key
+        return isNextChar ? [...prev, key] : prev
+      })
+    }
   }
 
   return (
     <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
-        <span className="matched"></span>
-        <span className="remainder">{word}</span>
+        <Word word={word} validKeys={validKeys} />
       </div>
       <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
       <div className="completed-words">
